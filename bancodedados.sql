7f2642d3ae744fb1a9502adf0175cc773c6e9d2f
Machado <machadogm14@gmail.com>
2025-08-03 14:30:48 -0300

    correção no sql

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE painel_consultas;
 
-use painel_consultas;
+USE painel_consultas;
 
 CREATE TABLE pacientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -9,15 +9,14 @@ CREATE TABLE pacientes (
     data_nascimento DATE NOT NULL,
     genero ENUM('M', 'F', 'O') NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE medicos (
-id INT AUTO_INCREMENT PRIMARY KEY,
-    id int AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     crm VARCHAR(20) UNIQUE NOT NULL,
-    especialidade VARCHAR(50) NOT NULL,
+    especialidade VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE consultas (
@@ -26,6 +25,7 @@ CREATE TABLE consultas (
     medico_id INT NOT NULL,
     data DATE NOT NULL,
     horario TIME NOT NULL,
+    observacoes TEXT,
     FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
     FOREIGN KEY (medico_id) REFERENCES medicos(id)
 );
